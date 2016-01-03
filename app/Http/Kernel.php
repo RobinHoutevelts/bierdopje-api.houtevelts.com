84945c0394750f5ae4d505aa3ea26eb61a17dd21
Robin Houtevelts <robin@houtevelts.com>
2016-01-03 19:06:50 +0100

    Enable CORS on all requests
    Extracted the config/cors.php file using:
    php artisan vendor:publish --provider="Barryvdh\Cors\ServiceProvider"

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,6 +18,7 @@ class Kernel extends HttpKernel
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
         //\App\Http\Middleware\VerifyCsrfToken::class, Not needed for an API
+        'Barryvdh\Cors\HandleCors',
         'Barryvdh\HttpCache\Middleware\CacheRequests',
     ];
 
